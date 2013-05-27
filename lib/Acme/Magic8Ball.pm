ceb59ef25a47bde6cd7447f2f15067411c60272d
Simon Wistow <simonw@cpan.org>
2013-05-27 22:53:50 +0000

    Import of SIMONW/Acme-Magic8Ball-1.3 from CPAN.
    gitpan-cpan-distribution: Acme-Magic8Ball
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         SIMONW/Acme-Magic8Ball-1.3.tar.gz
    gitpan-cpan-author:       SIMONW
    gitpan-cpan-maturity:     released

--- lib/Acme/Magic8Ball.pm
+++ lib/Acme/Magic8Ball.pm
@@ -1,14 +1,25 @@
 package Acme::Magic8Ball;
 
-use Exporter;
-use base qw(Exporter);
-use vars qw($VERSION @EXPORT_OK);
+use strict;
+
+require Exporter;
+use vars qw($VERSION $CONSISTENT @EXPORT_OK @ISA);
 
 
 
 # are we ever going to need enhancements? Apparently yes :(
-$VERSION   = "1.2"; 
-@EXPORT_OK = qw(ask);
+$VERSION    = "1.3"; 
+$CONSISTENT = 0;
+@ISA        = qw(Exporter);
+@EXPORT_OK  = qw(ask);
+
+use Data::Dumper;
+
+sub import {
+    $CONSISTENT = grep { /^:consistent$/ } @_;
+    @_ = grep { !/^:consistent$/ } @_;
+    goto &Exporter::import;
+}
 
 =head1 NAME
 
@@ -18,26 +29,46 @@ Acme::Magic8Ball - ask the Magic 8 Ball a question
 
     use Acme::Magic8Ball qw(ask);
     my $reply = ask("Is this module any use whatsoever?");
+    
+... you can also pass in your own list of answers ...
 
+    my $reply = ask("What should the next bit be?", 0, 1); # reply will always be 0 or 1
+
+... or make answers consistent ...
+
+    use Acme::Magic8Ball qw(ask :consistent);
+    for (0..1000) {
+        my $reply = ask("Is this module any use whatsoever?"); # reply will always be the same
+    }
+    
 =head1 DESCRIPTION
 
 This is an almost utterly pointless module. But I needed it. So there.
 
 =head1 METHODS
 
-=head2 ask <question>
+=head2 ask <question> [answers]
 
 Ask and ye shall receive!
+
+If you don't pass in an array of answers it will use the traditional ones.
 
 =cut
     
 sub ask {
     my $question = shift || return "You must ask a question!";
+    my @answers  = @_;
 
-    my $pos = tell DATA;
-    my @answers = map { chomp; $_ } <DATA>;
-    seek DATA, $pos,0;
-    return $answers[rand @answers ];
+    unless (@answers) {
+        my $pos = tell DATA;
+        @answers = map { chomp; $_ } <DATA>;
+        seek DATA, $pos,0;
+    }
+    return $answers[rand @answers] unless $CONSISTENT;
+
+    my $hashcode = 0;                                                                                                                                       
+    $hashcode   += ord($_) foreach split(//, $question);                                                                                                        
+    return $answers[$hashcode % scalar(@answers) - 1];
 }
 
 =head1 AUTHOR
@@ -49,6 +80,12 @@ Simon Wistow <simon@thegestalt.org>
 Copyright 2005, Simon Wistow
 
 Distributed under the same terms as Perl itself.
+
+=head1 DEVELOPMENT
+
+You can get the latest version from 
+
+https://github.com/simonwistow/Acme-Magic8Ball
 
 =head1 SEE ALSO
 
